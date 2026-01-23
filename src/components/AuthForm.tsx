834f2f38a022c996b93e72704b8da4ca5f0b5435
Yuriy <germanovich4job@gmail.com>
2026-01-23 15:53:02 +0300

    sdfs

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -9,15 +9,28 @@ import {
   Dialog,
   DialogTitle,
 } from "@mui/material"
+import axios from "axios"
 import { Link, useRouter } from "@tanstack/react-router"
 import { useLoginMutation, useRegisterMutation } from "../services/authApi"
 import { useState } from "react"
 
+const http = axios.create({
+  baseURL: "http://localhost:5000/api/",
+  params: {},
+  withCredentials: true,
+})
+
 // Определение схемы Zod для разных режимов
 const registerSchema = z.object({
-  name: z.string().min(2, "Минимальная длина имени - 2 символа").max(50),
+  username: z.string().min(2, "Минимальная длина имени - 2 символа").max(50),
+  firstName: z.string().min(2, "Минимальная длина имени - 2 символа").max(50),
+  lastName: z.string().min(2, "Минимальная длина имени - 2 символа").max(50),
   email: z.string().email(),
+  phone: z.string(),
   password: z.string().min(6, "Пароль должен содержать минимум 8 символов"),
+  passwordRepeat: z
+    .string()
+    .min(6, "Пароль должен содержать минимум 8 символов"),
 })
 
 const loginSchema = z.object({
@@ -26,6 +39,16 @@ const loginSchema = z.object({
 })
 
 const AuthForm = ({ mode }) => {
+  const handleFinish = async values => {
+    console.log("Success")
+
+    try {
+      const data = await http.post("auth/register", values)
+      console.log("data", data)
+    } catch (error) {
+      console.log("error", error)
+    }
+  }
   const { navigate } = useRouter()
 
   const [showErrorMessage, setShowErrorMessage] = useState(false)
@@ -42,6 +65,7 @@ const AuthForm = ({ mode }) => {
   const [login, { isLoading: isLoggingIn }] = useLoginMutation()
   const [register, { isLoading: isRegistering }] = useRegisterMutation()
 
+  console.log(methods.formState.errors)
   // Функция отправки формы
   const submitHandler = async data => {
     try {
@@ -67,14 +91,41 @@ const AuthForm = ({ mode }) => {
       <DialogContent>
         <form {...methods.handleSubmit(submitHandler)}>
           {mode === "register" && (
-            <TextField
-              required
-              fullWidth
-              label="Ваше имя"
-              name="name"
-              autoComplete="name"
-              {...methods.register("name")}
-            />
+            <>
+              {" "}
+              <TextField
+                required
+                fullWidth
+                label="username"
+                name="username"
+                autoComplete="username"
+                {...methods.register("username")}
+              />
+              <TextField
+                required
+                fullWidth
+                label="firstName"
+                name="fistName"
+                autoComplete="firstName"
+                {...methods.register("firstName")}
+              />
+              <TextField
+                required
+                fullWidth
+                label="lastName"
+                name="lastName"
+                autoComplete="lastName"
+                {...methods.register("lastName")}
+              />
+              <TextField
+                required
+                fullWidth
+                label="phone"
+                name="phone"
+                autoComplete="phone"
+                {...methods.register("phone")}
+              />
+            </>
           )}
           <TextField
             required
@@ -93,6 +144,17 @@ const AuthForm = ({ mode }) => {
             autoComplete="current-password"
             {...methods.register("password")}
           />
+          {mode === "register" && (
+            <TextField
+              required
+              fullWidth
+              label="Повтор пароля"
+              name="passwordRepeat"
+              type="passwordRepeat"
+              autoComplete="current-password"
+              {...methods.register("passwordRepeat")}
+            />
+          )}
         </form>
       </DialogContent>
       <DialogActions>
@@ -102,7 +164,7 @@ const AuthForm = ({ mode }) => {
         <Button
           variant="contained"
           color="primary"
-          onClick={methods.handleSubmit(submitHandler)}
+          onClick={methods.handleSubmit(handleFinish)}
         >
           {mode === "register" ? "Зарегистрироваться" : "Войти"}
         </Button>
