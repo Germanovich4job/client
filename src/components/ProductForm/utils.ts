54d1c97bbd2c816b8c221a57fb094de4c7bf79b9
Yuriy <germanovich4job@gmail.com>
2026-02-02 05:13:21 +0300

    вывыв

--- src/components/ProductForm/utils.ts
+++ src/components/ProductForm/utils.ts
@@ -1,7 +1,14 @@
 export const readImageAsBase64 = (file: File): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.onload = event => resolve(event.target.result.toString());
+    reader.onload = event => {
+      const target = event.target;
+      if (target && typeof target.result === "string") {
+        resolve(target.result);
+      } else {
+        reject(new Error("Error reading file"));
+      }
+    };
     reader.onerror = error => reject(error);
     reader.readAsDataURL(file);
   });
